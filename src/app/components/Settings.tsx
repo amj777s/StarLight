6bda74448a8ef0cf68bb2e1a521f81601dc0af9e
Kelly J <kvj8627@yahoo.com>
2023-09-12 13:08:49 -0500

    Added type literals to gameStatus. Updated Startscreen. Made Settings component.

---
+++ src/app/components/Settings.tsx
@@ -0,0 +1,10 @@
+export default function Settings() {
+
+    return (
+            <>
+               <div>Color: {Color}</div>
+               <div>Cursor:{Cursor} </div>
+               <div>Style: {Style} </div>
+            </>
+    )
+}
